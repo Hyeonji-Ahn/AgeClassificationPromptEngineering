68cc3eed11e5ead7cb92f72b54ea198d42ab4923
Hyeonji-Ahn <66628832+Hyeonji-Ahn@users.noreply.github.com>
2024-02-14 11:18:50 -0500

    ape setup

--- promptTuning.py
+++ promptTuning.py
@@ -1,19 +1,7 @@
 import openai
 import os
 import openpyxl
-import yaml
-
-from pathlib import Path
-import sys
-
-# Add the directory containing the file to the sys.path list
-file_path = Path(r"C:\Users\ahj28\Desktop\Stem Fellowship\automatic_prompt_engineer-main\automatic_prompt_engineer")
-sys.path.append(str(file_path))
-
-# Now you can import the modules
-import ape
-import config
-
+from automatic_prompt_engineer import ape
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
@@ -26,27 +14,47 @@ samples = []
 
 eval_template = \
 """Instruction: [PROMPT]
+
 Input: [INPUT]
 Output: [OUTPUT]"""
+
+prompt_gen_template = \
+"""
+Based on the instruction, a program produced the following input-output pairs:
+
+    [full_DEMO]
+    
+    The instruction was to [APE]
+"""
 
 
 textdata = openpyxl.load_workbook('DepressionText_parsed.xlsx')
 # Get all the sheets in the workbook
 sheets = textdata.sheetnames
 
-ws = textdata[sheets[0]]
-for row in ws.rows:
-    age.append(str(row[1].value))
-    samples.append(str(row[0].value))
 
-with open(os.path.join(os.path.dirname(__file__), "new_config.yaml")) as f:
-    new_config = yaml.safe_load(f)
-    
-config.update_config(new_config)
+# for i in range(5):
+#     # Get the active worksheet
+#     ws = textdata[sheets[i]]
+#     # Iterate through the rows in the worksheet
+#     for row in ws.rows:
+#         #print(row[1].value , " " ,row[0].value)
+#         samples.append([row[1].value,row[0].value])
+
+for sheet in sheets:
+    ws = textdata[sheet]
+    for row in ws.rows:
+        if(row[1].value == "age"):
+            continue
+        age.append(str(row[1].value))
+        samples.append(str(row[0].value))
 
 result, demo_fn = ape.simple_ape(
-    dataset=(age, samples),
+    dataset=(samples, age),
     eval_template=eval_template,
+    eval_model = "davinci-002",
+    prompt_gen_model = "davinci-002",
+    num_prompts=35
 )
 
 print(result)
